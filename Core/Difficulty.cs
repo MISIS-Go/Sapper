3cac21095d74e346ce1817292355968eb6a7840b
timap <tima.prenko@mail.ru>
2026-05-24 16:48:00 +0300

    some help

---
+++ Core/Difficulty.cs
@@ -0,0 +1,9 @@
+namespace Model.Core
+{
+    public static class Difficulty
+    {
+        public const double Easy = 0.20;
+        public const double Medium = 0.30;
+        public const double Hard = 0.40;
+    }
+}
